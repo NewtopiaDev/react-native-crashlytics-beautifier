fc803b7c25b43f4b3e09f19f75c493b3614d18e7
Chris Burger <cburger@newtopia.com>
2018-11-14 17:05:56 -0500

    Moved beautifier js code from app to module

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 import {NativeModules} from 'react-native';
-module.exports = NativeModules.RNStackBeautifier;
+
+module.exports = {
+    init: function() {
+        function parseErrorStack(e) {
+            if (!e || !e.stack) {
+                return [];
+            }
+            const stacktraceParser = require('stacktrace-parser');
+            const stack = Array.isArray(e.stack) ? e.stack : stacktraceParser.parse(e.stack);
+            let framesToPop = typeof e.framesToPop === 'number' ? e.framesToPop : 0;
+            while (framesToPop--) {
+                stack.shift();
+            }
+            return stack;
+        }
+
+        var originalHandler = global.ErrorUtils.getGlobalHandler();
+        function errorHandler(e, isFatal) {
+            let parsedStack = parseErrorStack(e);
+            let a = NativeModules.RNStackBeautifier.recordException(e.message, parsedStack, isFatal); 
+        }
+        global.ErrorUtils.setGlobalHandler(errorHandler);
+    }
+}
